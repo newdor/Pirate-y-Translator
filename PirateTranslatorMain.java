ca83a6614166179c4b6ae3dc619dfbc2c413d3d6
slg2522 <77139318+slg2522@users.noreply.github.com>
2021-03-10 14:01:37 -0500

    Add files via upload

---
+++ PirateTranslatorMain.java
@@ -0,0 +1,24 @@
+//Name: Rachael Devlaeminck
+//  Assignment: Lab 02
+//  Title: Pirate Translator
+//  Course: CSCE 270
+//  Lab Section: Section 01
+//  Semester: Fall 2010
+//  Instructor: David Wolff
+//  Date: 9/21/10
+//  Sources consulted: None
+//  Program description: translates what the use types into pirate
+//  Known Bugs: None
+//  Creativity: maintaining capitalization through translation using StringBuilder
+
+public class PirateTranslatorMain 
+{
+	public static void main(String[] args)
+	{
+		pirateTranslator translator = new pirateTranslator();
+		
+		translator.runInteraction();
+	}
+		
+	
+}
