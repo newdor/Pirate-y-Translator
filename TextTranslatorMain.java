3588a605f67c3b58eb1e8f5a573801e84a560161
newdor <newsha.doroudgarian@gmail.com>
2021-03-14 18:09:29 -0400

    added a different main for handling phrases

---
+++ TextTranslatorMain.java
@@ -0,0 +1,19 @@
+import java.util.Map;
+import java.util.Scanner;
+
+public class TextTranslatorMain {
+	public static void main(String[] args) {
+//add the whole text input into a String variable
+		Scanner keyboard = new Scanner(System.in);
+		String englishText = keyboard.next();
+//for every English word in the dictionary check the text and replace the word with pirate-y word
+
+		PirateTranslator captainHook = new PirateTranslator();
+		for (Map.Entry<String,String> entry : captainHook.getDictionary().entrySet()) {//using map.entrySet() for iteration
+			englishText = englishText.replaceAll(entry.getKey(), entry.getValue());
+		}
+		System.out.println(englishText);
+		keyboard.close(); // suggested by Eclipse for security
+	}
+
+}
