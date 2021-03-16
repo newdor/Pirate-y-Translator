2ec2c2e2561213eb49cfe4a8eb1cdd7b98f39dbf
slg2522 <77139318+slg2522@users.noreply.github.com>
2021-03-16 15:18:46 -0400

    Update PirateTranslatorMain.java

--- PirateTranslatorMain.java
+++ PirateTranslatorMain.java
@@ -1,14 +1,22 @@
+//imports the scanner for input
 import java.util.Scanner;
+
+//creates the main class for the translator
 public class PirateTranslatorMain {
 
 	public static void main(String[] args) {
+		
+		//creates the new intake field
 		PirateTranslator translator = new PirateTranslator();
 		Scanner scanner = new Scanner(System.in);
 		String word = "";
 		printOptions();
+		
+		//takes a single word input
 		System.out.print("enter an option: ");
 		int option = scanner.nextInt();
 		scanner.nextLine();
+		
 		// start the game loop
 		while (option!=2){
 			System.out.println("enter your text: ");
