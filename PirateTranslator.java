a85442ce5ca1a52aaf3a535b80014291b3c288ba
slg2522 <77139318+slg2522@users.noreply.github.com>
2021-03-16 15:42:34 -0400

    Update PirateTranslator.java
    Adds comments and clarifies loop terminology

--- PirateTranslator.java
+++ PirateTranslator.java
@@ -1,8 +1,13 @@
+//imports the program necessary to retrieve data from txt file
+//allows for hashmap
+//allows for scanner
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Scanner;
 
+//creates hashmap to access the dictionary
+//loads the rules method
 public class PirateTranslator {
 	private HashMap<String, String> dictionary;
 
@@ -19,29 +24,38 @@ public class PirateTranslator {
 
 			Scanner scan = new Scanner(file);
 
+			//instructs that text input is split by semi-colon with stirng as
+			//element 0 is the english string, element 1 is the pirate jargon
 			while (scan.hasNextLine()){
 				String line = scan.nextLine();
 				String[] sl = line.split(" ; ");
 				dictionary.put(sl[0], sl[1]);
 			}
 
-			scan.close();// suggested by Eclipse
+			//closes the scanner
+			scan.close();
 
 		}
+		
+		//if file is not found, return message and stop
 		catch (FileNotFoundException e) {
 			System.out.println("File not found.");
 			return;
 		}
 
+		//to test working dictionary, print entries
 		// System.out.println(dictionary);
 	}
 
+	//creates a loop to match the user input (stored as array) to the single dictionary elements
 	public String getTranslatedText(String words){
+		//splits the string around recognized portions
 		String [] wordArr = words.split(" ");
 		String res = "";
 		for (int i = 0; i<wordArr.length;i++){
 			res+=getWordMeaning(wordArr[i])+" ";
 		}
+		//takes spaces before and after away
 		return res.strip();
 	}
 
@@ -52,11 +66,12 @@ public class PirateTranslator {
 		}
 	}
 
-	// get the word meaning
+	//if word meaning is not in the dictionary, return the original english word
 	public String getWordMeaning(String word){
 		if (!dictionary.containsKey(word)){
 			return word;
 		}
+		//@return original english word 
 		return dictionary.get(word);
 	}
 
